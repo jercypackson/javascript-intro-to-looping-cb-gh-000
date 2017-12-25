ec93a4b8583de100d2c55f137ce4c0cc8c02056f
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 12:18:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,6 +14,10 @@ function whileLoop(n){
 }
 
 function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
   do {
     array.pop();
   } while (array.length > 0 && maybeTrue());
